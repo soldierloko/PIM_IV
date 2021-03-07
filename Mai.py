c14ad86ec6e8f1be4bf6cd0dc29c10e48646b503
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2021-03-07 12:30:59 -0300

    Intruções

---
+++ Mai.py
@@ -0,0 +1,57 @@
+# Projeto de um Sistema de reserva de equipamentos 
+# audiovisuais, que tem como propósito agilizar e controlar o empréstimo de equipamentos 
+# e recursos de apoio aos professores de colégios de Ensino Fundamental e Médio.
+
+# Elencar, argumentar e justificar sobre metodologias referentes à Análise de Sistemas, Qualidade de Software e Projeto de Interface com Usuário que são utilizadas nos 
+# projetos de sistemas computacionais;
+
+# 2.2. OBJETIVOS ESPECÍFICOS
+#  Desenvolver e aplicar os conhecimentos adquiridos nas disciplinas de: Economia e 
+# Mercado, Engenharia de Software II, Projeto de Interface com Usuários e Programação 
+# Orientada a Objetos I;
+#  Fomentar o hábito de executar projetos envolvendo múltiplas disciplinas;
+#  Desenvolver a capacidade de identificar necessidades e propor soluções técnicas;
+#  Elencar, argumentar e justificar sobre metodologias referentes à Análise de Sistemas, Qualidade de Software e Projeto de Interface com Usuário que são utilizadas nos 
+# projetos de sistemas computacionais;
+#  Aplicar as normas ABNT para a produção de trabalhos acadêmicos.
+# 2.3 CONTEXTUALIZAÇÃO DO CASO
+# Antigamente, o professor para ministrar suas aulas utilizava apenas giz, lousa, livros e seu 
+# conhecimento para apresentar o conteúdo de sua disciplina aos seus alunos. Considerando 23
+# Serviço Social
+# o aumento de informações a serem apresentadas em cada disciplina, o comportamento 
+# da nova geração dos alunos que são totalmente ligados à tecnologia e às ferramentas 
+# de interação humano-computador, a área educacional está aderindo gradativamente na 
+# utilização ferramentas audiovisuais para apoiar a dinâmica das aulas dos Colégios de Ensino 
+# Fundamental e Médio.
+# O Colégio Vencer Sempre disponibiliza equipamentos de informática e vídeo (tais como 
+# datashow, TV com VCR, TV com DVD, Projetor de Slides, Sistemas de Áudio-Microfone, 
+# Caixa Amplificada, Notebooks, Kits Multimídia etc.), como ferramentas de apoio para aulas 
+# e palestras, aos professores e coordenadores da instituição, alocando-os em salas de aula e 
+# auditórios, a pedido antecipado dos colaboradores.
+# O Sistema de reservas de empréstimo de equipamentos e recursos audiovisuais tem 
+# como propósito agilizar e controlar o empréstimo de equipamentos e recursos de apoio aos 
+# professores e dos demais colaboradores que necessitem utilizar os recursos mencionados.
+# 2.4 ATIVIDADES A SEREM DESENVOLVIDAS E O RELACIONAMENTO COM AS 
+# DISCIPLINAS
+# O aluno ou grupo de PIM deverá apresentar o projeto de sistema para reserva de 
+# equipamentos audiovisuais para um Colégio de Ensino Fundamental e Médio, contemplando 
+# os seguintes itens e relacionamento com as disciplinas:
+#  Com o auxílio da disciplina de Economia e Mercado, mapear quem serão os agentes 
+# econômicos que atuam diretamente com a sua empresa de software que fornecerá o 
+# sistema. Apresentar também a viabilidade econômica para a implementação do projeto 
+# do software, indicando além de outras informações relevantes, o prazo para a conclusão do projeto e o investimento financeiro necessário;24
+# Manual de Estágio
+#  Com o auxílio da disciplina de Engenharia de Software II, aluno ou grupo deve 
+# propor: Quais Requisitos Funcionais, Requisitos Não Funcionais e de Negócios serão 
+# necessários para a confecção deste sistema. Serão necessárias as especificações de Interfaces, das regras de negócio e das mensagens a serem exibidas;
+#  Sua empresa de software não possui muitos recursos financeiros, é nova no mercado, 
+# mas pretende trabalhar com metodologias de qualidade de software. Qual metodologia 
+# frente Normas Internacionais ISO, CMMI, MPS.br seria mais adequada à sua empresa de 
+# software? Selecione a opção mais adequada para este problema computacional, explique como a metodologia funciona no detalhe e justifique sua opção tecnicamente;
+#  Elabore um planejamento de teste para os Requisitos Funcionais e descreva Roteiro 
+# de Testes para os requisitos mencionados;
+#  Desenvolver protótipos de interfaces com alta fidelidade (em qualquer ferramenta) 
+# para os Requisitos Funcionais e explicar em detalhes o funcionamento de cada tipo de 
+# Entrada de Dados, Processamento ou Saída de Informação.
+#  Com o auxílio da disciplina de Programação Orientada a Objetos I, descreva 
+# tecnicamente os fundamentos acadêmicos sobre Objetos, Classes, Herança e Poliforfismo e identifique estes elementos técnicos em seu sistema.
