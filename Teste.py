4433e3ce7d734f11bb57253e872f9cd81ecbe6a7
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2021-03-13 19:54:57 -0300

    Teste

--- Teste.py
+++ Teste.py
@@ -18,6 +18,32 @@ CREATE TABLE  CLIENTES (
         criado_em DATE NOT NULL
 );""")
 
+# criando a tabela (schema)
+cursor.execute("""
+CREATE TABLE  TB_COMODATO (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        cpf     VARCHAR(11) NOT NULL,
+        email TEXT NOT NULL,
+        fone TEXT,
+        dtNascimento DATE NOT NULL,
+        classificacao INT NOT NULL,
+        criado_em DATE NOT NULL
+);""")
+
+# criando a tabela (schema)
+cursor.execute("""
+CREATE TABLE  TB_COMODATO (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        cpf     VARCHAR(11) NOT NULL,
+        email TEXT NOT NULL,
+        fone TEXT,
+        dtNascimento DATE NOT NULL,
+        classificacao INT NOT NULL,
+        criado_em DATE NOT NULL
+);""")
+
 print('Tabela criada com sucesso.')
 # desconectando...
 conn.close()
