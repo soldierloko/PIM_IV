476f971cf88a6ba9c8298cfba0501d47e2eac459
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2021-03-22 20:29:47 -0300

    Update teste

--- Teste1.py
+++ Teste1.py
@@ -1,3 +1,16 @@
-from datetime import date
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="wazeyes") # Nome do seu aplicativo
+location = geolocator.geocode("Abel Scuissiato, 2829 colombo paraná")
+print(location.address)
+print((location.latitude, location.longitude))
+# import getpass
+# from datetime import datetime
 
-print(type(date.today()))
+# print("Usuário.......: ", getpass.getuser())
+# print("Data Completa.: ", datetime.now())
+# print("Dia...........: ", datetime.now().day)
+# print("Mês...........: ", datetime.now().month)
+# print("Ano...........: ", datetime.now().year)
+# print("Hora..........: ", datetime.now().hour)
+# print("Minuto........: ", datetime.now().minute)
+# print("Segundo.......: ", datetime.now().second)
